5938052d68abf6c2afba9fc97519c25010191e6c
Agustin Radeland <amradeland@gmail.com>
2021-12-02 12:55:43 -0300

    segunda prueba

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -4,6 +4,7 @@ export function Logout(e) {
   console.log("DESLOGUEADO!!!");
   e.preventDefault();
   console.log("HOLA")
+  console.log("segunda prueba")
   return axios.post("/api/auth/logout");
 
 }
